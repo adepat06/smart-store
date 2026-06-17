938aec949447d7fc7fb81d374c0cc20e38543351
Adelin Patricia <lmindia04@Laymens-MacBook-Air.local>
2026-06-17 22:56:44 +0530

    Add default products

--- products/views.py
+++ products/views.py
@@ -4,12 +4,37 @@ from .models import Product
 
 def product_list(request):
 
+    if Product.objects.count() == 0:
+
+        Product.objects.create(
+            name="Nike Air Max",
+            description="Premium running shoe",
+            price=4999,
+            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff"
+        )
+
+        Product.objects.create(
+            name="Adidas Ultraboost",
+            description="Comfortable sports shoe",
+            price=6999,
+            image="https://images.unsplash.com/photo-1543508282-6319a3e2621f"
+        )
+
+        Product.objects.create(
+            name="Puma Velocity",
+            description="Lightweight performance shoe",
+            price=3999,
+            image="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519"
+        )
+
     products = Product.objects.all()
 
     return render(
         request,
         'products/product_list.html',
-        {'products': products}
+        {
+            'products': products
+        }
     )
 
 
@@ -23,5 +48,7 @@ def product_detail(request, product_id):
     return render(
         request,
         'products/product_detail.html',
-        {'product': product}
+        {
+            'product': product
+        }
     )
